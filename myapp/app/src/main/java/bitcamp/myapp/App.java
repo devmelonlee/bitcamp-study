67bbd2e73dd43318c0b110b3c93e35a3fb081add
devmelonlee <devmelonlee@gmail.com>
2023-05-26 21:24:36 +0900

    LF to CRLF Test

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -6,19 +6,22 @@ public class App {
     System.out.println("나의 목록 관리 시스템 "); // 스트링 리터럴
     System.out.println("----------------------------------------");
 
-    System.out.print("번호 : ");
-    System.out.println(100);
-    
-    System.out.printf("이름 : %s ", "홍길동");  // %s 자리에 문자열출력
-    System.out.println(); //줄만 바꾼다
+    int no = 100;
+    String name = "홍길동";
+    int age = 20;
+    boolean working = true;
+    float leftEye = 1.5f;
+    float rightEye = 1.5f;
 
-    System.out.println("나이 :  " + 20);  // 출력전 20을 문자열로 변경, 합쳐서 하나의 문자열로 전달
+    System.out.printf("번호 : %d\n", no);
+    System.out.printf("이름 : %s\n", name);
+    System.out.printf("나이 : %d\n", age);
+    System.out.printf("재직자 : %b\n", working);
+    System.out.printf("성별(남자(M), 여자(W)) : %c\n", gender);
+    System.out.printf("좌우시력 : %.1f, %.1f\n", leftEye, rightEye);
 
-    System.out.printf("재직자 : %b\n", true); // \n : 다음줄
-
-    System.out.printf("성별(남자(M), 여자(W)) : %c\n", 'M'); //문자 : ''
-
-    System.out.printf("좌우시력 : %.1f, %.1f\n", 1.5f, 1.2f);
-    
+    String[] name = new String[100];
+    name[0] = "aaa";
+    name[99] = "bbb";
   }
 }
