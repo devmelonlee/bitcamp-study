23a1bea071d0f2759d4018a56277cf0e6611c1b8
devmelonlee <devmelonlee@gmail.com>
2023-06-05 17:58:33 +0900

    Day10 code study

--- myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -1,18 +1,23 @@
 package bitcamp.myapp;
 
+// 소스코드에서 Calculator 클래스는 bitcamp.util 패키지에 소속된 클래스를 가리킨다.
+import bitcamp.util.Calculator;
+
 public class Test {
+
+    //static int result; // static 변수는 기본 값으로 0으로 초기화 된다.
     public static void main (String[] args){
-        System.out.println(args);
-        int a = 100, b = 200;
-        swap(a, b);
-        //call by value 값에의한 호출
-        System.out.printf("main() : %d, %d\n", a,b);
+        // 2 * 3 + 7 - 2 / 2 = ?
+        // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
+        // int result = ((2 *  3) + 7 - 2) / 2;
+
+        Calculator.init(2);
+        Calculator.multiple(3);
+        Calculator.plus(7);
+        Calculator.minus(2);
+        Calculator.divide(2);
+
+        System.out.println(Calculator.result); 
     }
 
-    static void swap(int a, int b) {
-        int tmp = a;
-        a = b;
-        b = tmp;
-        System.out.printf("swap() : %d, %d\n", a,b);
-    }
 }
