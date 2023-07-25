590ad7aa32e37d777105ce5008cb94048d566087
이승혁 <devmelonlee@gmail.com>
2023-07-25 17:54:26 +0900

    edit : Day44 Study update

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberListListener.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberListListener.java
@@ -1,5 +1,6 @@
 package bitcamp.myapp.handler;
 
+import java.io.IOException;
 import java.util.List;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
@@ -15,15 +16,14 @@ public class MemberListListener implements ActionListener {
   }
 
   @Override
-  public void service(BreadcrumbPrompt prompt) {
-    System.out.println("---------------------------------------");
-    System.out.println("번호, 이름, 이메일, 성별");
-    System.out.println("---------------------------------------");
+  public void service(BreadcrumbPrompt prompt) throws IOException {
+    prompt.println("---------------------------------------");
+    prompt.println("번호, 이름, 이메일, 성별");
+    prompt.println("---------------------------------------");
 
     List<Member> list = memberDao.list();
     for (Member m : list) {
-      System.out.printf("%d, %s, %s, %s\n",
-          m.getNo(), m.getName(), m.getEmail(),
+      prompt.printf("%d, %s, %s, %s\n", m.getNo(), m.getName(), m.getEmail(),
           m.getGender() == 'M' ? "남성" : "여성");
     }
   }
