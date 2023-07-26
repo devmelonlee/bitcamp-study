bca07f01b394e8cce1d0586eafa929c18e87bd0b
이승혁 <devmelonlee@gmail.com>
2023-07-26 23:35:15 +0900

    edit : Day46 Study update

--- myapp/app-server/src/main/java/bitcamp/dao/MySQLBoardDao.java
+++ myapp/app-server/src/main/java/bitcamp/dao/MySQLBoardDao.java
@@ -1,6 +1,5 @@
 package bitcamp.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -8,21 +7,22 @@ import java.util.List;
 import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.DataSource;
 
 public class MySQLBoardDao implements BoardDao {
 
-  Connection con;
+  DataSource ds;
   int category;
 
-  public MySQLBoardDao(Connection con, int category) {
-    this.con = con;
+  public MySQLBoardDao(DataSource ds, int category) {
+    this.ds = ds;
     this.category = category;
   }
 
   @Override
   public void insert(Board board) {
-    try (PreparedStatement stmt =
-        con.prepareStatement("insert into myapp_board(title,content,writer,password,category)"
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("insert into myapp_board(title,content,writer,password,category)"
             + " values(?,?,?,sha1(?),?)")) {
 
       stmt.setString(1, board.getTitle());
@@ -38,150 +38,115 @@ public class MySQLBoardDao implements BoardDao {
     }
 
   }
+
   /*
-  select
-    b.board_no,
-    b.title,
-    b.view_count,
-    b.created_date,
-    m.member_no,
-    m.name
-  from
-    myapp_board b inner join myapp_member m on b.writer=m.member_no
-  where
-    category=1
-  order by
-    board_no desc
-  */
- @Override
- public List<Board> list() {
-   try (PreparedStatement stmt = con.prepareStatement(
-       "select" +
-           "  b.board_no, " +
-           "  b.title, " +
-           "  b.view_count, " +
-           "  b.created_date, " +
-           "  m.member_no, " +
-           "  m.name " +
-           " from " +
-           "  myapp_board b inner join myapp_member m on b.writer=m.member_no" +
-           " where " +
-           "  category=?" +
-           " order by " +
-           "  board_no desc"
-       )) {
+   * select b.board_no, b.title, b.view_count, b.created_date, m.member_no, m.name from myapp_board
+   * b inner join myapp_member m on b.writer=m.member_no where category=1 order by board_no desc
+   */
+  @Override
+  public List<Board> list() {
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("select" + "  b.board_no, " + "  b.title, " + "  b.view_count, "
+            + "  b.created_date, " + "  m.member_no, " + "  m.name " + " from "
+            + "  myapp_board b inner join myapp_member m on b.writer=m.member_no" + " where "
+            + "  category=?" + " order by " + "  board_no desc")) {
 
-     stmt.setInt(1, this.category);
+      stmt.setInt(1, this.category);
 
-     try (ResultSet rs = stmt.executeQuery()) {
-       List<Board> list = new ArrayList<>();
-       while (rs.next()) {
-         Board b = new Board();
-         b.setNo(rs.getInt("board_no"));
-         b.setTitle(rs.getString("title"));
-         b.setViewCount(rs.getInt("view_count"));
-         b.setCreatedDate(rs.getTimestamp("created_date"));
+      try (ResultSet rs = stmt.executeQuery()) {
+        List<Board> list = new ArrayList<>();
+        while (rs.next()) {
+          Board b = new Board();
+          b.setNo(rs.getInt("board_no"));
+          b.setTitle(rs.getString("title"));
+          b.setViewCount(rs.getInt("view_count"));
+          b.setCreatedDate(rs.getTimestamp("created_date"));
 
-         Member writer = new Member();
-         writer.setNo(rs.getInt("member_no"));
-         writer.setName(rs.getString("name"));
-         b.setWriter(writer);
+          Member writer = new Member();
+          writer.setNo(rs.getInt("member_no"));
+          writer.setName(rs.getString("name"));
+          b.setWriter(writer);
 
-         list.add(b);
-       }
-       return list;
-     }
+          list.add(b);
+        }
+        return list;
+      }
 
-   } catch (Exception e) {
-     throw new RuntimeException(e);
-   }
- }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
 
- @Override
- public Board findBy(int no) {
-   try (PreparedStatement stmt = con.prepareStatement(
-       "select" +
-           "  b.board_no, " +
-           "  b.title, " +
-           "  b.content," +
-           "  b.view_count, " +
-           "  b.created_date, " +
-           "  m.member_no, " +
-           "  m.name " +
-           " from " +
-           "  myapp_board b inner join myapp_member m on b.writer=m.member_no" +
-           " where " +
-           "  category=?" +
-           "  and board_no=?"
-       )) {
+  @Override
+  public Board findBy(int no) {
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("select" + "  b.board_no, " + "  b.title, " + "  b.content,"
+            + "  b.view_count, " + "  b.created_date, " + "  m.member_no, " + "  m.name " + " from "
+            + "  myapp_board b inner join myapp_member m on b.writer=m.member_no" + " where "
+            + "  category=?" + "  and board_no=?")) {
 
-     stmt.setInt(1, this.category);
-     stmt.setInt(2, no);
+      stmt.setInt(1, this.category);
+      stmt.setInt(2, no);
 
-     try (ResultSet rs = stmt.executeQuery()) {
-       if (rs.next()) {
-         Board b = new Board();
-         b.setNo(rs.getInt("board_no"));
-         b.setTitle(rs.getString("title"));
-         b.setContent(rs.getString("content"));
-         b.setViewCount(rs.getInt("view_count"));
-         b.setCreatedDate(rs.getTimestamp("created_date"));
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (rs.next()) {
+          Board b = new Board();
+          b.setNo(rs.getInt("board_no"));
+          b.setTitle(rs.getString("title"));
+          b.setContent(rs.getString("content"));
+          b.setViewCount(rs.getInt("view_count"));
+          b.setCreatedDate(rs.getTimestamp("created_date"));
 
-         Member writer = new Member();
-         writer.setNo(rs.getInt("member_no"));
-         writer.setName(rs.getString("name"));
-         b.setWriter(writer);
+          Member writer = new Member();
+          writer.setNo(rs.getInt("member_no"));
+          writer.setName(rs.getString("name"));
+          b.setWriter(writer);
 
-         stmt.executeUpdate("update myapp_board set"
-             + " view_count=view_count + 1"
-             + " where board_no=" + no);
+          stmt.executeUpdate(
+              "update myapp_board set" + " view_count=view_count + 1" + " where board_no=" + no);
 
-         return b;
-       }
-       return null;
-     }
-   } catch (Exception e) {
-     throw new RuntimeException(e);
-   }
- }
+          return b;
+        }
+        return null;
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
- @Override
- public int update(Board board) {
-   try (PreparedStatement stmt = con.prepareStatement(
-       "update myapp_board set"
-           + " title=?,"
-           + " content=?"
-           + " where category=? and board_no=? and writer=?")) {
+  @Override
+  public int update(Board board) {
+    try (PreparedStatement stmt = ds.getConnection(false).prepareStatement("update myapp_board set"
+        + " title=?," + " content=?" + " where category=? and board_no=? and writer=?")) {
 
-     stmt.setString(1, board.getTitle());
-     stmt.setString(2, board.getContent());
-     stmt.setInt(3, this.category);
-     stmt.setInt(4, board.getNo());
-     stmt.setInt(5, board.getWriter().getNo());
+      stmt.setString(1, board.getTitle());
+      stmt.setString(2, board.getContent());
+      stmt.setInt(3, this.category);
+      stmt.setInt(4, board.getNo());
+      stmt.setInt(5, board.getWriter().getNo());
 
-     return stmt.executeUpdate();
+      return stmt.executeUpdate();
 
-   } catch (Exception e) {
-     throw new RuntimeException(e);
-   }
- }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
- @Override
- public int delete(Board board) {
-   try (PreparedStatement stmt = con.prepareStatement(
-       "delete from myapp_board"
-           + " where category=? and board_no=? and writer=?")) {
+  @Override
+  public int delete(Board board) {
+    try (PreparedStatement stmt = ds.getConnection(false).prepareStatement(
+        "delete from myapp_board" + " where category=? and board_no=? and writer=?")) {
 
-     stmt.setInt(1, this.category);
-     stmt.setInt(2, board.getNo());
-     stmt.setInt(3, board.getWriter().getNo());
+      stmt.setInt(1, this.category);
+      stmt.setInt(2, board.getNo());
+      stmt.setInt(3, board.getWriter().getNo());
 
-     return stmt.executeUpdate();
+      return stmt.executeUpdate();
 
-   } catch (Exception e) {
-     throw new RuntimeException(e);
-   }
- }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
 }
